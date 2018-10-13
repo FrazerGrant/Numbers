fdc6feee4f3c170f86740fc4f43123ea18b317ce
Frazer Grant <fgrant@wndirect.com>
2018-10-13 16:47:32 +0100

    how many time is a number divisable by another number

--- 100 3/Program.cs
+++ 100 3/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _100_3
 {
@@ -10,6 +6,19 @@ namespace _100_3
     {
         static void Main(string[] args)
         {
+            int count = 0;
+            int max = 100;
+            int divisable = 3;
+            for (int value = max; value >= 1; value-- )
+            {
+                if (value % divisable == 0)
+                {
+                    Console.WriteLine(value);
+                    count++;
+                }
+            }
+            Console.WriteLine("Number of times {0} is divisable by {1} is {2}", max, divisable, count);
+         Console.ReadKey();
         }
     }
 }
